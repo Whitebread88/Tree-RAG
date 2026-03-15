1f74acdfe6409f49e3a4e6cdf815d5fd9c9653dc
Aaron Foong <foongpro@gmail.com>
2026-03-15 21:28:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+from typing import Optional
+from fastapi import FastAPI, Depends
+from sqlmodel import SQLModel, Field, Session, select
+from .database import engine, get_session
 
+app = FastAPI()
