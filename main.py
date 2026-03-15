8ba2a7779189b53c3472e3c4d4af794c32bffe5f
Aaron Foong <foongpro@gmail.com>
2026-03-15 21:42:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from typing import Optional
 from fastapi import FastAPI, Depends
-from sqlmodel import SQLModel, Field, Session, select
+from sqlmodel import SQLModel, Session, select
+from contextlib import asynccontextmanager
 from .database import engine, get_session
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # This triggers the connection to Cloud SQL immediately on startup
+    # If this fails, you will see a CLEAR error in the Cloud Run logs
+    SQLModel.metadata.create_all(engine)
+    yield
 
 app = FastAPI()
 
